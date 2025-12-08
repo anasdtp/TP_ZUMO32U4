d9c15b435d79dba353e44e3e427a9a61e9a4acf3
Ines <ines.mechti@universite-paris-saclay.fr>
2025-12-08 16:42:54 +0100

    ajout de blt

---
+++ lib/Blt/bluetoot.cpp
@@ -0,0 +1,40 @@
+/*This demo requires a Zumo 32U4 waitForButton
+*** Configuration :  
+	pour programmer le bluetooth
+	avant d'allumer le zumo, maintenir le bouton du bluetooth appuyé
+	allumer le zumo
+	attendez quelques secondes
+	appuyez sur le bouton B
+	Première musique
+	deuxieme musique, c'est bon le blue tooth est programmé
+	vous pouvez maintenant travailler
+	pas besoin de refaire l'opération
+*/
+
+#include <Wire.h>
+#include <Zumo32U4.h>
+
+Zumo32U4Buzzer buzzer;
+Zumo32U4ButtonB buttonB;
+
+
+//*****************************************************
+// STEUP initialisations
+//****************************************************
+void setup() {
+  Serial1.begin(38400);
+  buzzer.play("L16 cdegreg4"); //petite musique pour la route
+  while(buzzer.isPlaying());
+	buttonB.waitForButton(); // on attend d'appuyer sur le bouton B
+	delay(800); //attendre pour ne pas risquer de blaisser le doigt
+
+ Serial1.println("AT+NAME=ZUPR");// mettez votre nom ici
+  delay(800);
+  Serial1.println("AT+UART=38400,0,0");
+ buzzer.play("L16 reg4cdeg"); //petite musique pour la route
+  while(buzzer.isPlaying());
+}
+
+void loop(){
+
+}
